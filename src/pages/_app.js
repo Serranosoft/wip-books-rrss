12a27e04f3f041f9b16457627109d19bc6d2a1a7
Manuel Scholz <manuel.serrano943@gmail.com>
2024-08-04 16:55:45 +0200

    Fix: Mover header a _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import Header from "@/layout/header";
 import "@/styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Header />
+            <Component {...pageProps} />
+        </>
+
+    )
 }
