ef5039cf4d363ec24c4baf25de3aa0648cf08445
Manuel Scholz <manuel.serrano943@gmail.com>
2024-08-06 20:44:37 +0200

    WIP: Pruebas webhook para imprimir la request en logs de vercel

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -4,6 +4,21 @@ import { supabase } from "@/layout/layout";
 
 
 export default async function handler(req, res) {
-    const { body } = req;
-    return res.send(`Parsed request body: ${body} / ${body.name}`);
+  if (req.method === 'POST') {
+    console.log(req.body);
+    // res.status(200).end(`${req.body}`);
+    // const { title, author, description, otherFields } = req.body;
+
+    // Inserta el nuevo libro en la base de datos de Supabase
+    // const { data, error } = await supabase.from('books').insert([{ title, author, description, otherFields }]);
+
+    // if (error) {
+    //   return res.status(500).json({ error: error.message });
+    // }
+
+    // return res.status(200).json({ data });
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
