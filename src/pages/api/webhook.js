c59ebd3801b0bb4fb109f46b0720ea6b84541b2c
Manuel Scholz <manuel.serrano943@gmail.com>
2024-08-07 15:27:33 +0200

    WIP: añadir registro a libros al recibir webhook

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,24 +1,15 @@
-// pages/api/webhook.js
-
 import { supabase } from "@/layout/layout";
 
-
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    console.log(req.body);
-    res.status(200).end(`good job`);
-    // const { title, author, description, otherFields } = req.body;
+    if (req.method === 'POST') {
+        console.log(req.body);
 
-    // Inserta el nuevo libro en la base de datos de Supabase
-    // const { data, error } = await supabase.from('books').insert([{ title, author, description, otherFields }]);
+        const { error } = await supabase.from("books").insert({ id: req.post_id });
 
-    // if (error) {
-    //   return res.status(500).json({ error: error.message });
-    // }
-
-    // return res.status(200).json({ data });
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+        if (!error) return res.status(200);
+        return res.status(500).json({ error: error.message });;       
+    } else {
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
 }
