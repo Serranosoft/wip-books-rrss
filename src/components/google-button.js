7058a83884e802575e137fdc96d0eaaa1bd1b16e
Manuel Scholz <manuel.serrano943@gmail.com>
2024-08-13 21:22:37 +0200

    Fix: Corregir funcion google login

--- src/components/google-button.js
+++ src/components/google-button.js
@@ -1,9 +1,9 @@
-import { login } from "@/controller/database/user";
+import { login_auth } from "@/controller/database/user";
 import styles from "@/styles/components/google-button.module.scss";
 
 export default function GoogleButton() {
 
     return (
-        <button type="button" className={styles.button} onClick={login}>Iniciar sesión con Google</button>
+        <button type="button" className={styles.button} onClick={login_auth}>Iniciar sesión con Google</button>
     )
 }
