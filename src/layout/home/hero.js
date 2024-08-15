c7e8e22209e42ef662d3fe6ee2800a9056f51539
Manuel Scholz <manuel.serrano943@gmail.com>
2024-08-15 20:06:38 +0200

    Fix: Corrección texto home

--- src/layout/home/hero.js
+++ src/layout/home/hero.js
@@ -8,7 +8,7 @@ export default function Hero() {
             <div className={styles.title}>
                 <span className={styles.info}>¡Más de 10.000 libros disponibles!</span>
                 <h1>El brillante destino para los lectores</h1>
-                <span className={styles.subheading}>Disfruta del trabajo de millones de autores, escribe y conoce la opinión de otros autores y lectores.</span>
+                <span className={styles.subheading}>Disfruta del trabajo de millones de autores, escribe y conoce la opinión de otros lectores.</span>
                 <Button>Ver más</Button>
             </div>
         </div>
