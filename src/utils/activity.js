dfd4615fdb7e803233a0c916f5716e20b33e33d8
Manuel Scholz <manuel.serrano943@gmail.com>
2024-09-02 18:21:35 +0200

    Feat: Añadir slug del libro referido en la actividad de usuario

--- src/utils/activity.js
+++ src/utils/activity.js
@@ -1,14 +1,15 @@
 /** Formatea una actividad de reseña */
 export function formatReview(review) {
+    console.log(review);
     let result = {
         user: review.users.name,
         userSlug: review.users.slug,
         avatar: review.users.image,
         userTitle: review.users.title,
         book: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1705592447l/199440249._SY475_.jpg",
-        bookUrl: `/libro/${review.books.name}`
+        bookUrl: `${review.books.slug}`
     }
-    let activity = `Ha escrito una valoración de ${review.rating} estrellas en el <a href="/libro/${review.books.name}">libro ${review.books.name}</a>`;
+    let activity = `Ha escrito una valoración de ${review.rating} estrellas en el ${review.books.name} libro ${review.books.name}`;
     activity += getDaysSinceReview(review.published) > 1 ? ` hace ${getDaysSinceReview(review.published)} días` : " recientemente";
     result.activity = activity;
     return result;
